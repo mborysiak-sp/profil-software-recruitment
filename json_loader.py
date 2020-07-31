e117146c11c79dca5a64cca5849a4396b04dc77c
marcinez-sp <marcinez24@gmail.com>
2020-07-31 14:08:43 +0200

    Added functions for editing json file

--- json_loader.py
+++ json_loader.py
@@ -1,10 +1,13 @@
 import json
 import sqlite3
+import re
+import datetime
 
 class JsonLoader:
 
     def __init__(self):
         self.data = {}
+
 
     def load_file(self):
         filename = "persons.json"
@@ -12,7 +15,41 @@ class JsonLoader:
         with open(filename, "r+") as file:
             self.data = json.load(file)
     
+
     def modify_file(self):
         if self.data is not None:                
             for element in self.Data:
                 element.pop("picture", None)
+                self.fix_numbers(element)
+                self.add_days_to_birthday(element)
+
+
+    def fix_numbers(self, element):
+        if element["phone"] is not None:
+           element["phone"] = self.remove_special_characters(element["phone"])
+        if element["cell"] is not None:
+           element["cell"] = self.remove_special_characters(element["cell"])
+
+
+    def remove_special_characters(self, number):
+        result = re.sub("[^0-9]+", "", number)
+        return result
+
+
+    def add_days_to_birthday(self, element):
+        if element is not None:
+            element["dtb"] = self.calculate_days_to_birthday(element["dob"])
+
+
+    def calculate_days_to_birthday(self, dob):
+        current_date = datetime.date.today()
+        dtb = self.days_difference(current_date, dob)
+        return dtb
+
+
+    def days_difference(self, d1, d2):
+        date_format = "%Y-%m-%d"
+        d1 = datetime.strptime(d1, date_format)
+        d2 = datetime.strptime(d2, date_format)
+        return abs(d1 - d2)
+        
