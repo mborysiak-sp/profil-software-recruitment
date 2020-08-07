0cd5a21bd2a0b9f469f3d6679c002a421a56339d
marcinez-sp <marcinez24@gmail.com>
2020-08-07 16:02:37 +0200

    Edited README.md -> Small changes Edited json_loader.py -> Added docstrings and some comments, changed how days to birthday are calculated to give right amount when birthday was next year

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## Description
 It is a console project made for recruitment purposes. It loads json data based on https://randomuser.me/ api,
-manipulates it, inserts it into sqlite database using peewee ORM and then get's user requested data through CLI
-based on click.
+manipulates it, inserts it into sqlite database using <b>peewee</b> ORM and then get's user requested data through CLI
+based on <b>click</b> module.
 
 ## Requirements
 <ul>
@@ -29,13 +29,13 @@ $ pip install -r requirements.txt
 
 ## Usage
 
-Run a command:
+Run a {command}:
 ```pythonregexp
-$ cli command
+$ python cli {command}
 ```
 
 List of commands:
 ```pythonregexp
-$ cli --help
+$ python cli --help
 ```
 
--- json_loader.py
+++ json_loader.py
@@ -7,57 +7,99 @@ from api import Api
 
 
 class JsonLoader:
-
+    """Class that loads and modifies data stored in json format"""
     def __init__(self):
         self.data = {}
 
     def load_file(self, filename):
+        """Load specified filename into the object data field"""
         with open(filename, 'r', encoding="utf-8") as file:
             self.data = json.load(file)
 
     def load_data_from_api(self, n):
+        """
+        Load specified amount of users from predefined api
+        :param n: number of users to be loaded
+        """
         self.data["results"] = []
         for i in range(0, int(n)):
             self.data["results"].append((Api.get("https://randomuser.me/api/")["results"][0]))
 
     def save_file(self, filename):
+        """
+        Saves data stored currently in data as json file.s
+        :param filename: Name of file to be saved to
+        """
         with open(filename, 'w') as file:
             json.dump(self.data, file)
 
     def modify_file(self):
+        """
+        Modifies data stored in object's data field by deleting every "picture" field, removing special characters
+        from numbers and calculating days to person's birthday
+        """
         if self.data is not None:
             for element in self.data["results"]:
+                # This dictionary method removes 'picture' element and if it's not present, inserts 'None'
                 element.pop("picture", None)
                 self.fix_numbers(element)
                 self.add_days_to_birthday(element)
 
-    def remove_special_characters(self, number):
+    def remove_not_numbers(self, number):
+        """
+        Removes all characters save numbers from given number string
+        :param number: string containing a number
+        :return: Number with only numbers
+        """
         result = re.sub("[^0-9]+", "", number)
         return result
 
     def fix_numbers(self, element):
+        """
+        If number related fields are not empty, it calls a method to remove special characters from them
+        :param element: Single person from data dictionary
+        """
         if element["phone"] is not None:
-            element["phone"] = self.remove_special_characters(element["phone"])
+            element["phone"] = self.remove_not_numbers(element["phone"])
         if element["cell"] is not None:
-            element["cell"] = self.remove_special_characters(element["cell"])
+            element["cell"] = self.remove_not_numbers(element["cell"])
 
     def days_difference(self, d1, d2):
-        if type(d1) is str:
-            d1 = string_to_date(d1, "%Y-%m-%dT%H:%M:%S.%fZ")
-        if type(d2) is str:
-            d2 = string_to_date(d2, "%Y-%m-%dT%H:%M:%S.%fZ")
-            d2 = date(datetime.now().year, d2.month, d2.day)
+        """
+        Calculates difference between two given dates in days
+        :param d1: First date
+        :param d2: Second date
+        :return: Calculated days between dates
+        """
         result = (d1 - d2).days
         if result < 0:
             result = 365 - abs(result)
         return result
 
     def calculate_days_to_birthday(self, dob):
+        """
+        Converts "dob" parameter from string to date and changes it's year to the current one. Then
+        :param dob: date of birth dictionary field from data which stores "date" key inside
+        :return: Calculated days to
+        """
         current_date = date.today()
-        dtb = self.days_difference(current_date, dob["date"])
+        # Get's next birthday date as a current year to get not exact
+        birthday_str = dob["date"]
+        d = string_to_date(birthday_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+        next_birthday_date = date(datetime.now().year, d.month, d.day)
+        # If person had birthday in this year, we calculated days to the next year's one by incrementing current year
+        if current_date < next_birthday_date:
+            next_birthday_date.year = next_birthday_date.year + 1
+        # Call method calculating days to birthday
+        dtb = self.days_difference(current_date, next_birthday_date)
+        # Cut string to get only needed days
         dtb = str(dtb).split(',')
         return dtb
 
     def add_days_to_birthday(self, element):
+        """
+        Inserts into given element parameter calculated days to birthday
+        :param element: element which should receive a new key "dtb" with days to birthday as it's value
+        """
         if element is not None:
             element["dtb"] = self.calculate_days_to_birthday(element["dob"])
